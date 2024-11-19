92987d9403101116080f725fe6c13cf101480474
Soma <c0a23045d8@edu.teu.ac.jp>
2024-11-19 15:45:18 +0900

    こうかとんを召喚

--- pygame_image.py
+++ pygame_image.py
@@ -9,13 +9,16 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.image.load("fig/pg_bg.jpg") # 背景画像Surfaceを作成
+    koukaton_3 = pg.image.load("fig/3.png")
+    koukaton_3 = pg.transform.flip(koukaton_3, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #screen surfaceに背景画像Surfaceを張り付ける
+        screen.blit(koukaton_3, [300, 200]) #screen syrfaceにkoukatonイメージを張り付ける
         pg.display.update()
         tmr += 1        
         clock.tick(10)
